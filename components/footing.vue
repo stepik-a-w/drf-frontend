ed039561d493c1b4af02a7071687568eb88f081b
gkatejka <kate.golovkina@gmail.com>
2021-01-28 15:08:55 +0300

    Added footer

---
+++ components/footing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-navbar class="page-footer" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/" class="page-footer-logo">
+        <b-img src="../assets/images/logo.png" alt="Stepik Packages" width="190"></b-img>
+      </b-navbar-brand>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#" class="px-3">О Коробочках</b-nav-item>
+        <b-nav-item href="#" class="px-3">Полезное</b-nav-item>
+        <b-nav-item href="#" class="px-3">Сладкое</b-nav-item>
+        <b-nav-item href="#" class="pl-3">Новое</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .page-footer {
+    padding-top: .8rem;
+    padding-bottom: .8rem;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    border-radius: 4px;
+
+    &.bg-dark {
+      background-color: #222516 !important;
+
+      .nav-link {
+        font-size: 18px;
+        color: #fff;
+        text-decoration: underline;
+      }
+    }
+
+    
+  }
+</style>
