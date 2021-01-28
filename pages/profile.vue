7ba1f8bc4b8dc570ce00680806c39612dfda5c4a
gkatejka <kate.golovkina@gmail.com>
2021-01-29 01:08:38 +0300

    Added profile page

---
+++ pages/profile.vue
@@ -0,0 +1,169 @@
+<template>
+  <div>
+    <b-row class="pb-5">
+      <b-col cols="8">
+        <h1 class="h3 mb-5">История заказов</h1>
+        <b-card no-body class="mb-4">
+          <b-list-group flush>
+            <b-list-group-item class="px-4 py-3">
+              <b-row class="order-bar">
+              <b-col class="order-number" cols="3"># 45353544</b-col>
+              <b-col class="order-date" cols="4">21 декабря 14:05</b-col>
+              <b-col class="order-price">5 900 Р</b-col>
+              <b-col class="order-status"><b-badge variant="success">Выполняется</b-badge></b-col>
+            </b-row>
+            </b-list-group-item>
+            <b-list-group-item class="text-muted">Товарная позиция 1, Товарная позиция 2, Товарная позиция 3</b-list-group-item>
+          </b-list-group>
+        </b-card>
+        <b-card no-body class="mb-4">
+          <b-list-group flush>
+            <b-list-group-item class="px-4 py-3">
+              <b-row class="order-bar">
+              <b-col class="order-number" cols="3"># 453532343</b-col>
+              <b-col class="order-date" cols="4">18 декабря 21:17</b-col>
+              <b-col class="order-price">3 700 Р</b-col>
+              <b-col class="order-status"><b-badge variant="success">Выполнен</b-badge></b-col>
+            </b-row>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+        <b-card no-body class="mb-4">
+          <b-list-group flush>
+            <b-list-group-item class="px-4 py-3">
+              <b-row class="order-bar">
+              <b-col class="order-number" cols="3"># 453514299</b-col>
+              <b-col class="order-date" cols="4">12 декабря 18:17</b-col>
+              <b-col class="order-price">16 300 Р</b-col>
+              <b-col class="order-status"><b-badge variant="warning">Отменен</b-badge></b-col>
+            </b-row>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </b-col>
+      <b-col cols="4">
+        <h2 class="h3 mb-5">Профиль</h2>
+        <b-form @submit="onSubmit" @reset="onReset" class="profile-form px-4">
+          <b-form-group
+            id="fieldset-1"
+            label="Имя:"
+            label-for="input-1"
+            :invalid-feedback="invalidFeedback"
+            :state="state"
+          >
+            <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="fieldset-2"
+            label="Фамилия:"
+            label-for="input-2"
+            :invalid-feedback="invalidFeedback"
+            :state="state"
+          >
+            <b-form-input id="input-2" v-model="name" :state="state" trim></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="fieldset-3"
+            label="Отчество:"
+            label-for="input-3"
+            :invalid-feedback="invalidFeedback"
+            :state="state"
+          >
+            <b-form-input id="input-3" v-model="name" :state="state" trim></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="fieldset-4"
+            label="Телефон:"
+            label-for="input-4"
+            :invalid-feedback="invalidFeedback"
+            :state="state"
+          >
+            <b-form-input id="input-4" v-model="tel" :state="state" trim></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary" size="lg" class="mt-3">Сохранить</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  layout: 'default',
+
+  data() {
+    return {
+      pagename: "Профиль"
+    }
+  },
+
+  // тестовый запрос для демонстрации возможностей
+
+  async asyncData({ $axios }) {
+
+    let planet = await $axios.$get('https://swapi.dev/api/planets/1');
+    console.log(planet)
+    return { planet }
+
+  },
+
+  methods: {
+    loadData: function() {
+      this.$axios.$get('https://swapi.dev/api/planets/2').then((result) => {
+        console.log(result)
+      })
+
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.order {
+  &-bar {
+    font-size: 18px;
+    line-height: 24px;
+
+    .col {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
+  &-number,
+  &-date,
+  &-price {
+    font-weight: bold;
+  }
+  &-number {
+    color: #1B76EA;
+  }
+  &-price {
+    text-align: center;
+  }
+  &-status {
+    text-align: right;
+    .badge {
+      font-size: 18px;
+      font-weight: 500;
+
+      &.badge-warning {
+        color:#fff;
+      }
+    }
+  }
+}
+
+.profile-form {
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  background-color: #F5F5F5;
+  border-radius: 4px;
+
+  label {
+    padding-bottom: 5px;
+  }
+}
+</style>
