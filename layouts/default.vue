6fe3f55551e521b500b25a72ac1eab83c5484964
gkatejka <kate.golovkina@gmail.com>
2021-01-28 15:09:14 +0300

    Added default template

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-container>
+      <heading />
+      <Nuxt />
+      <footing />
+    </b-container>
+  </div>
+</template>
+
+<script>
+import heading from "~/components/heading.vue";
+import footing from "~/components/footing.vue";
+
+export default {
+  components: {
+    heading,
+    footing
+  }
+};
+</script>
+
+<style lang="scss">
+/*
+
+  Здесь стили для основного лэйаута
+
+  */
+</style>
